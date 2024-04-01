d4775c67bb825c19036948f82a7f0d9650a19a76
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-04-01 20:50:05 -0300

    Inserindo comentário

--- tarefa_classes/src/main/java/com/rafael/Main.java
+++ tarefa_classes/src/main/java/com/rafael/Main.java
@@ -3,8 +3,10 @@ package com.rafael;
 public class Main {
     public static void main(String[] args) {
 
-        var cachorro = new Cachorro("Labrador", 25F, "Caramelo", "Thor");
+        System.out.println(media(20,10,7,3));
+    }
 
-        cachorro.latir();
+    public static double media(double numero1, double numero2, double numero3, double numero4){
+        return (numero1 + numero2 + numero3 + numero4) / 4 ;
     }
 }
