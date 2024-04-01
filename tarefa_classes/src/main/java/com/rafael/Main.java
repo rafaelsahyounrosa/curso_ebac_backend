1e80dbc9c34a153519ba42a2892be9ad918aeb61
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-04-01 20:50:49 -0300

    Finalizando exercicio

--- tarefa_classes/src/main/java/com/rafael/Main.java
+++ tarefa_classes/src/main/java/com/rafael/Main.java
@@ -3,7 +3,7 @@ package com.rafael;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(media(20,10,7,3));
+        System.out.println(media(20,11,7,3));
     }
 
     public static double media(double numero1, double numero2, double numero3, double numero4){
