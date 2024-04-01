e0b5b16c3fb27ca0e13aec63156435ab80caa12b
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-04-01 19:13:35 -0300

    Exercicio

---
+++ tarefa_classes/src/main/java/com/rafael/Main.java
@@ -0,0 +1,10 @@
+package com.rafael;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var cachorro = new Cachorro("Labrador", 25F, "Caramelo", "Thor");
+
+        cachorro.latir();
+    }
+}
