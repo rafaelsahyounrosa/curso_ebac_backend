97feb88a27c86a5436874a46e7fc860bab27a521
Rafael Sahyoun Rosa <134002700+rosa-rafael@users.noreply.github.com>
2024-06-30 13:46:37 -0300

    Exercicio MD13

--- EBAC_BE/src/main/java/Main.java
+++ EBAC_BE/src/main/java/Main.java
@@ -6,44 +6,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Digite a quantidade de pessoas que você deseja informar: ");
-        int quantidade = sc.nextInt();
-        sc.nextLine();
-
-        List<String> homens = new ArrayList();
-        List<String> mulheres = new ArrayList();
-
-        System.out.println("\nInforme as pessoas no seguinte modelo:\nNome - Sexo");
-
-        for (int i = 0; i < quantidade; i++) {
-
-            System.out.println("Informe a pessoa " + (i+1) + ": ");
-
-            String pessoa = sc.nextLine();
-
-            String nome = pessoa.split("-")[0].trim();
-            String sexo = pessoa.split("-")[1].trim();
-
-//            System.out.println(Arrays.toString(pessoa.split("-")));
-//            System.out.println(nome);
-//            System.out.println(sexo);
-
-            if (sexo.equalsIgnoreCase("homem")) {
-                homens.add(nome);
-                System.out.println("Adicionando à lista dos homens");
-            }else if (sexo.equalsIgnoreCase("mulher")) {
-                mulheres.add(nome);
-                System.out.println("Adicionando à lista das mulheres");
-            }
-        }
-
-        sc.close();
-        Collections.sort(homens);
-        Collections.sort(mulheres);
-        System.out.println(homens);
-        System.out.println(mulheres);
 
     }
 }
