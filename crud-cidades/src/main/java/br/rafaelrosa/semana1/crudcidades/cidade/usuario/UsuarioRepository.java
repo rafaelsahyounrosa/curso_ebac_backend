f4054d734bb16364d06c32ff624da67f1203595d
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2025-01-10 12:31:58 -0300

    Delete crud-cidades directory

--- crud-cidades/src/main/java/br/rafaelrosa/semana1/crudcidades/cidade/usuario/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.rafaelrosa.semana1.crudcidades.cidade.usuario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-    public Usuario findByNome(String nome);
-}
